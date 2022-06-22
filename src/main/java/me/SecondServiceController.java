dcb326ba723d3e2488e8015fee897ac2393babe5
sombrero104 <sombrero.khy@gmail.com>
2022-06-22 21:08:05 +0900

    .

--- src/main/java/me/SecondServiceController.java
+++ src/main/java/me/SecondServiceController.java
@@ -1,6 +1,7 @@
 package me;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,12 @@ public class SecondServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        System.out.println(header);
+        return "Hello World in Second Service.";
     }
 
 }
